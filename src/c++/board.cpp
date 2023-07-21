69452f4f4a46601459da682f768037183b73ba1f
ramgmtk <ramgmtk@gmail.com>
2023-07-21 00:55:44 -0700

    Created tic tac to board state class

---
+++ src/c++/board.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "board.h"
+
+//construct a base board
+board_state::board_state() {
+    empty = '#';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            this->this_board[i][j] = this->empty;
+            this->remaining_spots_array[i * 3 + j] = i * 3 + j;
+        }
+    }
+    this->remaining_spots = 9;
+}
+
+//copy values from the board state
+board_state::board_state(const board_state& b) {
+    empty = '#';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            this->this_board[i][j] = b.this_board[i][j];
+            this->remaining_spots_array[i * 3 + j] = b.remaining_spots_array[i * 3 + j];
+        }
+    }
+    this->remaining_spots = b.remaining_spots;
+}
+
+//sets value and updates the remaining spots
+//true boolean corresponds to 'O', false to 'X'
+void board_state::set_value(unsigned int x, unsigned int y, bool b) {
+    if (x >= 3 || y >= 3) std::cout << __func__ << ": Bad input provided." << std::endl;
+    if (this->this_board[x][y] != empty) std::cout << __func__ << ": The space at [" << x << ","
+        << y << "] is occupied." << std:: endl;
+    char value = b ? 'O' : 'X';
+    int new_spot = x * 3 + y;
+    for (int i = 0; i < remaining_spots; i++) {
+        if (remaining_spots_array[i] == new_spot) {
+            remaining_spots_array[i] = remaining_spots_array[remaining_spots - 1];
+            remaining_spots -= 1;
+            break;
+        }
+    }
+}
+
+//output the board
+void board_state::print() {
+    std::cout << "_______" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        std::cout << "|";
+        for (int j = 0; j < 3; j++) {
+            std::cout << this->this_board[i][j] << "|";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "_______" << std::endl;
+}
---
+++ src/c++/board.h
@@ -0,0 +1,34 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+//Board State class manages the data ata  given state for a tic tac toe board;
+class board_state {
+    private:
+        //empty space character
+        char empty;
+        //the actual board
+        char this_board[3][3];
+        //The remaining spots to be filled
+        //The array is used to ekep account of which wpots in the board are open. the index value does
+        //necessarily indicate the position on the board. We adjust remaining spots to limit how much of the array we see
+        //as all used values bet pushed to the back of the array.
+        int remaining_spots_array[9];
+        int remaining_spots;
+
+    public:
+        //constructors destructores
+        board_state();
+        board_state(const board_state&);
+        board_state& operator=(const board_state&) = delete;
+        ~board_state();
+        //setter
+        void set_value(unsigned int x, unsigned int y, bool b);
+        //print
+        void print();
+
+};
+
+class board {
+    friend class board_state;
+};
+#endif
